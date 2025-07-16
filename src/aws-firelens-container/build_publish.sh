fa3f0c3da10e8a61ed17eddc872c026503e57455
Ashley Gould <agould@ucop.edu>
2025-07-15 18:12:03 -0700

    continue with aws-firelens-container
    I am able to modify fluent-bit.conf using an include file.
    Almost working, but I need to modify the name of key log.level as it
    is errors at opensearch index stage.

--- src/aws-firelens-container/build_publish.sh
+++ src/aws-firelens-container/build_publish.sh
@@ -1,4 +1,5 @@
 # !bin/bash
+set -x
 ENV=$1
 ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 ECR_ID=$ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
@@ -7,12 +8,14 @@ if [ "$ENV" = '' ]; then
 else
   ECR_REPO_NAME="dmptool-${ENV}/aws-firelens"
 fi
+IMAGE_TAG="development"
 
 # Login to ECR
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ECR_ID
 # Build the image
-docker buildx build --platform linux/amd64 -t $ECR_REPO_NAME  --load .
+docker buildx build --platform linux/amd64 -t $ECR_REPO_NAME:$IMAGE_TAG  --load .
 # Tag the image
-docker tag $ECR_REPO_NAME:latest $ECR_ID/$ECR_REPO_NAME:latest
+docker tag $ECR_REPO_NAME:$IMAGE_TAG $ECR_ID/$ECR_REPO_NAME:$IMAGE_TAG
+#docker tag $ECR_REPO_NAME:latest $ECR_ID/$ECR_REPO_NAME:$IMAGE_TAG
 # Push the image up to ECR
-docker push $ECR_ID/$ECR_REPO_NAME:latest
+docker push $ECR_ID/$ECR_REPO_NAME:$IMAGE_TAG
